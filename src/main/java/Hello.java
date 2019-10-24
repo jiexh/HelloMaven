3790ba4f212eb09b1f6cbb293bd7f5d552a3d6eb
Jie Xue <christina.jiexue@gmail.com>
2019-10-24 00:24:14 -0700

    update hello

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,4 +1,10 @@
 public class Hello {
+
+    public static void main(String[] args) {
+        Hello h1 = new Hello();
+        System.out.println(h1.add(1, 8));
+    }
+
     public Hello() {
         System.out.println("hello");
     }
